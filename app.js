0fef6ac542e50167dc2c99c75f157d2d3556fff0
Fuad <Fuadmikayil>
2024-06-29 11:23:08 +0400

    random add

---
+++ app.js
@@ -0,0 +1,13 @@
+/*let age = 18;
+let username = "Fuad";
+let userSurname = "Mikayilov";
+let userage = 15;
+let userİsRich = false;
+let userHasCar = true;
+console.log(`Name: ${username}`)
+console.log(`Surname: ${userSurname}`)
+console.log(`Age: ${userage}`)
+console.log(`Evlidirmi: ${userİsRich}`)
+console.log(`Car: ${userHasCar}`)
+*/
+let a= Math.random();
